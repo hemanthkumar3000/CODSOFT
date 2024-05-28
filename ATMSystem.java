ab9c39955d718dbb728a10153883ca2d0f81f6c5
hemanthkumar3000 <132996925+hemanthkumar3000@users.noreply.github.com>
2024-05-28 10:19:19 +0530

    Delete ATMSystem.java

--- ATMSystem.java
+++
@@ -1,7 +0,0 @@
-public class ATMSystem {
-    public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000.0); // Initial balance
-        ATM atm = new ATM(account);
-        atm.start();
-    }
-}
